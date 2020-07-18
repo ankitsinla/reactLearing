a79861945c8071df21148ef1f1a4a938a4371f2e
ankitsinla <ankitsinla@gmail.com>
2020-07-18 10:26:38 +0530

    9 props basics

--- Greet.js
+++ Greet.js
@@ -4,6 +4,13 @@ import React from 'react'
 //     return <h1>hello ankit</h1>
 // }
 
-const Greet = () => <h1>hello ankit</h1>
+const Greet = (props) => {              //<Greet name="ankit" hero="superman"><p>class prop</p></Greet>
+    return (                            //return only one element
+        <div>                           
+            <h1>hello {props.name} is {props.hero}</h1>
+            {props.children}            //things within blocks
+        </div>
+    )
+}
 
 export default Greet
