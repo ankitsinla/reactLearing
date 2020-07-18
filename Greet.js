bada2572b42177ee2ab94bac3643fd9ae746fed2
ankitsinla <ankitsinla@gmail.com>
2020-07-18 09:43:26 +0530

    function component

--- Greet.js
+++ Greet.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-function Greet(){
-    return <h1>hello ankit</h1>
-}
+// function Greet(){
+//     return <h1>hello ankit</h1>
+// }
+
+const Greet = () => <h1>hello ankit</h1>
 
 export default Greet
