c49a1c7e799c99a4a69454c663b02a529870fae9
ankitsinla <54257704+ankitsinla@users.noreply.github.com>
2020-07-20 08:24:10 +0530

    Update Column.js

--- Column.js
+++ Column.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Column() {
     return(
-        <React.Fragment>   
+        <React.Fragment>                    //Fragment in place of div as div may cause error sometime 
             <td>name</td>
             <td>Ankit</td>
         </React.Fragment>
