1fa364e493bfeff93c5b9034a0462bca0230fe75
ankitsinla <ankitsinla@gmail.com>
2020-07-22 08:52:37 +0530

    RefDemo- highlight input on page loading

--- RefDemo.js
+++ RefDemo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class RefDemo extends Component{
     constructor(props){
         super(props)
-        this.inputRef = React.createRef()   //first method
+        this.inputRef = React.createRef()   //first method 
         this.cbRef = null                   //second method call back ref approach
         this.setCbRef = element => {
             this.cbRef =element
